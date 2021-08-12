7ac77d87a8370c2eefd8212721698562edbaeb24
GaboHep <86680331+GaboHep@users.noreply.github.com>
2021-08-11 19:39:25 -0500

    Creación FXML

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.proyecto2p {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.proyecto2p to javafx.fxml;
     exports com.mycompany.proyecto2p;
