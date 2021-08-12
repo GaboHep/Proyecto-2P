d00ecb9ff7bcbafc9c68f8458f881e4e68c19721
niplinig <niplinig@tutanota.com>
2021-08-11 19:44:30 -0500

    Commit

--- src/main/java/com/mycompany/proyecto2p/MundoController.java
+++ src/main/java/com/mycompany/proyecto2p/MundoController.java
@@ -1,27 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.mycompany.proyecto2p;
+package com.mycompany.mavenproject1;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 
-/**
- * FXML Controller class
- *
- * @author ggabo
- */
 public class MundoController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private FlowPane flwPn;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        fillGrid(5, 5);
+    }
+    
+    public void fillGrid(int Columns, int Rows) {
+        
+        flwPn.setAlignment(Pos.CENTER);
+        
+        GridPane gridpane = new GridPane();
+        
+        if (Columns > 0 && Rows > 0) {
+            
+            Image grassTile = new Image("/assets/grassTile.png", 50, 50, true, true);
+            
+            for (int x = 0; x < Columns; x++) {
+                for (int y = 0; y < Rows; y++) {
+                    ImageView imgvw = new ImageView(grassTile);
+                    gridpane.add(imgvw, x, y);
+                }
+            }
+            
+        }
+        
+        flwPn.getChildren().add(gridpane);
+    }
     
 }
