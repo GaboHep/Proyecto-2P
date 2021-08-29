8512adf792dea2694800517f48d2a10d19f6e3bb
GaboHep <86680331+GaboHep@users.noreply.github.com>
2021-08-29 15:30:32 -0500

    Agregados atributos de ciudad

--- src/main/java/com/mycompany/proyecto2p/Ciudad.java
+++ src/main/java/com/mycompany/proyecto2p/Ciudad.java
@@ -1,5 +1,34 @@
 package com.mycompany.proyecto2p;
 
+import java.util.ArrayList;
+
 public class Ciudad {
+    private String nombre;
+    private String nombreAlcalde;
+    private String tiempo;
+    private int presupuesto;
+    private ArrayList <Servicio> servicios=new ArrayList<>();
+
+    public Ciudad(String nombre, String nombreAlcalde, int presupuesto) {
+        this.nombre = nombre;
+        this.nombreAlcalde = nombreAlcalde;
+        this.presupuesto = presupuesto;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setNombreAlcalde(String nombreAlcalde) {
+        this.nombreAlcalde = nombreAlcalde;
+    }
+
+    public void setPresupuesto(int presupuesto) {
+        this.presupuesto = presupuesto;
+    }
+    
+    public void disminuirPresupuesto(int cantidad){
+        presupuesto=presupuesto-cantidad;
+    }
     
 }
