ffd65224f4e25bbcfe835e8aabaeb10c81abf1cc
niplinig <niplinig@tutanota.com>
2021-08-29 13:12:40 -0500

    Añadi las carpeta archivos y servicios

--- src/main/java/com/mycompany/proyecto2p/Partida.java
+++ src/main/java/com/mycompany/proyecto2p/Partida.java
@@ -1,14 +1,5 @@
 package com.mycompany.proyecto2p;
 
-import java.io.BufferedWriter;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import javafx.scene.control.TextField;
 
