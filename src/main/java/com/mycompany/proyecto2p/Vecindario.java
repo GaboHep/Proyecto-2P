61a2ea76894d74577f5a37643d74812a27681f1c
niplinig <niplinig@tutanota.com>
2021-08-29 17:07:54 -0500

    Clase Vecindario

--- src/main/java/com/mycompany/proyecto2p/Vecindario.java
+++ src/main/java/com/mycompany/proyecto2p/Vecindario.java
@@ -1,17 +1,87 @@
 package com.mycompany.proyecto2p;
 
+import java.util.ArrayList;
+
 public class Vecindario {
-    
+
     int radioVecindario = Configuracion.VECINDARIO;
-    
-    public void buscarVecindario(int indiceColumna, int indicieFila) {
-        
-        for (int i = 0; i < radioVecindario; i++) {
-            
-            
-            
+
+    public void buscarVecindario(int indiceColumna, int indiceFila) {
+
+        ArrayList <Integer> listaCeldasOcupadas = new ArrayList<>();
+
+        if (indiceColumna - radioVecindario >= 0 && indiceFila - radioVecindario >= 0) {
+
+            for (int x = indiceColumna - radioVecindario; x < indiceColumna + radioVecindario; x++) {
+
+                for (int y = indiceFila - radioVecindario; y < indiceFila + radioVecindario; y++) {
+
+//                    if (celda[x,y].isOcupada()) {
+//
+//                        listaCeldasOcupadas.add(celda[x,y]);
+//
+//                    }
+
+                }
+
+            }
+
         }
-        
+
+        else if (indiceColumna - radioVecindario >= 0 && indiceFila - radioVecindario == 0) {
+
+            for (int x = indiceColumna - radioVecindario; x < indiceColumna + radioVecindario; x++) {
+
+                for (int y = indiceFila; y < indiceFila + radioVecindario; y++) {
+
+//                    if (celda[x,y].isOcupada()) {
+//
+//                        listaCeldasOcupadas.add(celda[x,y]);
+//
+//                    }
+
+                }
+
+            }
+
+        }
+
+        else if (indiceColumna - radioVecindario == 0 && indiceFila - radioVecindario >= 0) {
+
+            for (int x = indiceColumna; x < indiceColumna + radioVecindario; x++) {
+
+                for (int y = indiceFila - radioVecindario; y < indiceFila + radioVecindario; y++) {
+
+//                    if (celda[x,y].isOcupada()) {
+//
+//                        listaCeldasOcupadas.add(celda[x,y]);
+//
+//                    }
+
+                }
+
+            }
+
+        }
+
+        else {
+
+            for (int x = indiceColumna; x < indiceColumna + radioVecindario; x++) {
+
+                for (int y = indiceFila; y < indiceFila + radioVecindario; y++) {
+
+//                    if (celda[x,y].isOcupada()) {
+//
+//                        listaCeldasOcupadas.add(celda[x,y]);
+//
+//                    }
+
+                }
+
+            }
+
+        }
+
     }
-    
+
 }
